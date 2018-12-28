489f2bc3d0f4544129d406777e561e05fd58bef0
Guillaume Simonneau <2980507+khezen@users.noreply.github.com>
2018-12-28 20:19:41 +0100

    sall for consumers

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/khezen/bulklog/config"
 	"github.com/khezen/bulklog/server"
 )
 
+const (
+	maxTries    = 30
+	retryPeriod = 5 * time.Second
+)
+
+var (
+	quit  chan error
+	cfg   *config.Config
+	serv  *server.Server
+	err   error
+	timer *time.Timer
+	i     int
+)
+
 func main() {
-	quit := make(chan error)
+	quit = make(chan error)
 	var err error
-	cfg, err := config.Get()
+	cfg, err = config.Get()
 	if err != nil {
 		panic(err)
 	}
-	server, err := server.New(cfg, quit)
+	for {
+		serv, err = server.New(cfg, quit)
+		if err != nil {
+			if i < maxTries {
+				fmt.Println(err)
+				timer = time.NewTimer(retryPeriod)
+				<-timer.C
+				i++
+			} else {
+				panic(err)
+			}
+		} else {
+			break
+		}
+	}
 	if err != nil {
 		panic(err)
 	}
-	go server.ListenAndServe()
+	go serv.ListenAndServe()
 	err = <-quit
 	panic(err)
 }
