d7395b40c7bdc05a151c86e22d3eee517b6d57d5
Guillaume Simonneau <2980507+khezen@users.noreply.github.com>
2019-01-06 20:15:03 +0100

    ci

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+unit_tests(){
+    local ret
+    ret=0
+    for d in $(go list ./... | grep -v vendor); do 
+        COV=$(go test -race  -coverprofile=profile.out -covermode=atomic $d | sed 's/.*coverage//g' | sed  's/[^0-9.]*//g') 
+        if [ -f profile.out ]; then 
+            cat profile.out >> coverage.txt;
+            rm profile.out;
+        fi 
+        if [[ ( $COV < 75.0 ) ]]; then  
+            echo expecting test coverage greater than 75 %, got insufficient $COV % for package $d; 
+            if test $ret -eq 0; then
+                ret=1
+            fi
+        fi
+    done
+    echo $ret
+    return $ret
+}
+
+set -e
+unit_tests
