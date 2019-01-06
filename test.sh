fda3a782b8b7e28e91a345cdd78e6c30bdd914f3
Guillaume Simonneau <2980507+khezen@users.noreply.github.com>
2019-01-06 20:47:20 +0100

    ci

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ unit_tests(){
     local ret
     ret=0
     for d in $(go list ./... | grep -v vendor); do 
-        COV=$(go test -race  -coverprofile=profile.out -covermode=atomic $d | sed 's/.*coverage//g' | sed  's/[^0-9.]*//g') 
+        COV=$(go test -race  -coverprofile=profile.out -covermode=atomic $d | sed 's/.*\[no test files\]/0/g' | sed 's/.*coverage//g' | sed  's/[^0-9.]*//g' | sed  's/\.[0-9.]*//g') 
         if [ -f profile.out ]; then 
             cat profile.out >> coverage.txt;
             rm profile.out;
         fi 
-        if [[ $COV < 75.0 ]]; then  
+        if test $COV -lt 75; then  
             echo expecting test coverage greater than 75 %, got insufficient $COV % for package $d; 
             if test $ret -eq 0; then
                 ret=1
